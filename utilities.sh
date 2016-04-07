c70c6374ed079df564cd742924fb5aef0733c23f
Fábio Lima <lr.fabioml@gmail.com>
2016-04-06 23:49:59 -0400

    Some utilities

---
+++ utilities.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install htop
+sudo apt-get install terminator
+sudo apt-get install g++
+sudo apt-get install git
+sudo apt-get install vim
+sudo apt-get install gitk
